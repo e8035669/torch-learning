0a2ce05538974946cd68dff25637ea531d3b251b
Yu-Ren Zhang <e8035669@gmail.com>
2021-12-06 23:18:17 +0800

    update

--- python/09-train-classify/docker-run.sh
+++ python/09-train-classify/docker-run.sh
@@ -3,6 +3,13 @@
 proxy=${http_proxy}
 if [ -n "${proxy}" ]; then
     p_export="export http_proxy=${proxy} https_proxy=${proxy} &&"
+    echo "with proxy"
 fi
 
-sudo docker run -it -v ${PWD}/model:/app/model -p 5000:5000 continuumio/miniconda3 bash -c "${p_export} pip install mlflow && mlflow models serve -m /app/model -p 5000 -h 0.0.0.0"
+gpu=""
+if [ -e /dev/nvidia0 ]; then
+    gpu="--gpus all"
+    echo "with gpu"
+fi
+
+sudo docker run -it ${gpu} -v ${PWD}/model:/app/model -p 5000:5000 continuumio/miniconda3 bash -c "${p_export} pip install mlflow && mlflow models serve -m /app/model -p 5000 -h 0.0.0.0"
--- python/09-train-classify/image_pyfunc.py
+++ python/09-train-classify/image_pyfunc.py
@@ -7,7 +7,7 @@ import torchvision
 import torchvision.transforms as T
 
 from PIL import Image
-
+import pandas as pd
 import mlflow
 import mlflow.keras
 from mlflow.utils import PYTHON_VERSION
@@ -48,32 +48,54 @@ def save_pytorch_model(pytorch_model, artifact_path, image_dims, domain):
             conda_env=conda_env
         )
 
+def load_image(img_data: bytes):
+    with BytesIO(img_data) as fd:
+        img = Image.open(fd)
+        img.load()
+    return img
+
+
 class PytorchClassifierWrapper:
-    def __init__(self, model, device):
+    def __init__(self, model, device, labels, dims):
         self.model = model
         self.device = device
+        self.labels = labels
+        self.dims = dims
+        if dims[1] == 3:
+            self.transforms = T.Compose([
+                T.Resize(dims[2:]),
+                T.ToTensor()
+            ])
+        else:
+            self.transforms = T.Compose([
+                T.Grayscale(),
+                T.Resize(dims[2:]),
+                T.ToTensor()
+            ])
 
 
-    def predict(self, x):
-        print(type(x))
-        print(x)
+    def predict(self, x: pd.DataFrame):
+        x = x.values.reshape(-1)
+        x = [load_image(base64.decodebytes(bytearray(img, encoding='utf-8'))) for img in x]
+        x = torch.cat([torch.unsqueeze(self.transforms(img), 0) for img in x])
+        print(x.shape)
 
-        datas = base64.decodebytes(bytearray(x[0], encoding='utf-8'))
-        fd = ByteIO(datas)
-        img = Image.open(fd)
+        y = self.model(x)
 
-        print(img.shape)
-        pass
+        result = torch.argmax(y, dim=1)
+        cls = [self.labels[r] for r in result]
+        print(cls)
 
-
-
-
+        return result.numpy()
 
 def _load_pyfunc(path):
     print('path', path)
     with open(os.path.join(path, 'conf.yaml'), 'r') as f:
         conf = yaml.safe_load(f)
     print('conf', conf)
+
+    labels = conf['domain'].split('/')
+    dims = [int(i) for i in conf['image_dims'].split('/')]
 
     model_path = os.path.join(path, 'torch_model')
     print('model_path', model_path)
@@ -82,7 +104,8 @@ def _load_pyfunc(path):
     print('device', device)
 
     net = mlflow.pytorch.load_model(model_path, map_location=device)
+    net.eval()
     print(net)
 
-    return net
+    return PytorchClassifierWrapper(net, device, labels, dims)
 
