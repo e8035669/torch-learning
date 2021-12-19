7f2b42e299e93adea093a9c70ab118a7cc392f52
Yu-Ren Zhang <e8035669@gmail.com>
2021-12-19 19:16:33 +0800

    update

--- python/09-train-classify/image_pyfunc.py
+++ python/09-train-classify/image_pyfunc.py
@@ -23,15 +23,19 @@ def get_pytorch_env_patch():
     find_torch = tuple(filter(lambda p: 'torch' in p, find_pip[0]['pip']))
     for p in find_torch:
         find_pip[0]['pip'].remove(p)
+    find_python = tuple(filter(lambda p: 'python' in p, e['dependencies']))
+    e['dependencies'].remove(find_python[0])
+    e['dependencies'].append('python')
     return e
 
-def save_pytorch_model(pytorch_model, artifact_path, image_dims, domain):
+def save_pytorch_model(pytorch_model, artifact_path, image_dims, domain, batch_limit = 16):
     with TempDir() as tmp:
         data_path = tmp.path('image_model')
         os.mkdir(data_path)
         conf = {
             'image_dims': '/'.join(map(str, image_dims)),
             'domain': '/'.join(map(str, domain)),
+            'batch_limit': batch_limit,
         }
         with open(os.path.join(data_path, 'conf.yaml'), 'w') as f:
             yaml.safe_dump(conf, stream=f)
@@ -57,11 +61,12 @@ def load_image(img_data: bytes):
 
 
 class PytorchClassifierWrapper:
-    def __init__(self, model, device, labels, dims):
+    def __init__(self, model, device, labels, dims, batch_limit):
         self.model = model
         self.device = device
         self.labels = labels
         self.dims = dims
+        self.batch_limit = batch_limit
         if dims[1] == 3:
             self.transforms = T.Compose([
                 T.Resize(dims[2:]),
@@ -75,31 +80,42 @@ class PytorchClassifierWrapper:
             ])
 
 
-    def predict(self, x: pd.DataFrame):
-        x = x.values.reshape(-1)
-        x = [load_image(base64.decodebytes(bytearray(img, encoding='utf-8'))) for img in x]
-        x = torch.cat([torch.unsqueeze(self.transforms(img), 0) for img in x])
-        print(x.shape)
+    def predict(self, data: pd.DataFrame):
+        data = data.values.reshape(-1)
+        data = [load_image(base64.decodebytes(bytearray(img, encoding='utf-8'))) for img in data]
+        data = torch.cat([torch.unsqueeze(self.transforms(img), 0) for img in data])
+        chunks = torch.split(data, self.batch_limit, 0)
+        print(data.shape)
+
+        confidences = []
+        classes = []
 
         with torch.no_grad():
-            x = x.to(self.device)
-            y = self.model(x)
-            y = F.softmax(y, 1)
-            conf, cls = y.max(1)
+            for x in chunks:
+                x = x.to(self.device)
+                y = self.model(x)
+                y = F.softmax(y, 1)
+                conf, cls = y.max(1)
+                confidences.append(conf)
+                classes.append(cls)
 
-        conf = conf.cpu().numpy()
-        cls = cls.cpu().numpy()
-        cls_name = [ self.labels[i] if i < len(self.labels) else "" for i in cls]
+        confidences = torch.cat(confidences, 0)
+        classes = torch.cat(classes, 0)
+
+        confidences = confidences.cpu().numpy()
+        classes = classes.cpu().numpy()
+        cls_name = [ self.labels[i] if i < len(self.labels) else "" for i in classes]
 
         result = [{
             "conf": c,
             "cls": l,
             "cls_name": n
-        } for c, l, n in zip(conf, cls, cls_name)]
+        } for c, l, n in zip(confidences, classes, cls_name)]
 
         print(result)
 
         return result
+
 
 def _load_pyfunc(path):
     print('path', path)
@@ -109,6 +125,7 @@ def _load_pyfunc(path):
 
     labels = conf['domain'].split('/')
     dims = [int(i) for i in conf['image_dims'].split('/')]
+    batch_limit = int(conf['batch_limit'])
 
     model_path = os.path.join(path, 'torch_model')
     print('model_path', model_path)
@@ -120,5 +137,5 @@ def _load_pyfunc(path):
     net.eval()
     print(net)
 
-    return PytorchClassifierWrapper(net, device, labels, dims)
+    return PytorchClassifierWrapper(net, device, labels, dims, batch_limit)
 
